8e0f4ad5a98808ca000bb1c3486d492111174a83
H4CK3R <H4CK3R@Python.com>
2021-04-10 14:32:25 +0200

    Added more scan functionality and updated to version 2

--- pymap.py
+++ pymap.py
@@ -4,19 +4,47 @@ import concurrent.futures
 from time import time
 from sys import argv
 
-if len(argv) != 3:
+ports = [20, 21, 22, 23, 25, 53, 65, 67, 68, 69, 88, 80, 110, 123, 135, 137, 138, 139, 143, 161, 162, 389, 443, 445, 500, 993, 995, 1433, 1433, 8080, 8088, 8443, 1494, 2512, 2513, 2598, 2869, 3389, 4500, 2222, 2020, 2022]
+version = "2.0"
+
+
+if "-v" in argv or "--version" in argv:
+    print("pymap version: " + str(version))
+    exit(0)
+
+if len(argv) < 3:
 	print("USAGE: ")
-	print("[IP] [STARTPORT-ENDPORT]")
+	print("""pymap [OPTIONS] [IP]
+              -sU | use UDP scan.
+              -sT | use TCP scan.
+              -sV | try to find the versions of ports.
+    -v, --version | print version of pymap.""")
 	exit(1)
 
-ports = argv[2].split('-')
-ip = socket.gethostbyname(argv[1])
+if "-sT" in argv and "-sU" in argv:
+    print("You can only use -sU or -sT!\nQUITTING!")
+    exit(1)
+
+if "-sT" in argv:
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+elif "-sU" in argv:
+    soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+else:
+    print("Please use -sT or -sU\nQUITTING!")
+    exit(-1)
+
+ip = socket.gethostbyname(argv[-1])
 
 ts = time()
 
-def scan(ip, port):
-    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    soc.settimeout(6)
+def scan(ip, port, soc):
+    soc.settimeout(2)
+    res = soc.connect_ex((ip, port))
+    if res == 0:
+        print("Port " + str(port) + " OPEN []")
+
+def scanVersion(ip, port, soc):
+    soc.settimeout(2)
     res = soc.connect_ex((ip, port))
 
     if res == 0:
@@ -39,9 +67,39 @@ def scan(ip, port):
 
         print("Port " + str(port) + ' OPEN [' + banner.rstrip() + "]")
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-	for port in range(int(ports[0]), int(ports[1]) + 1):
-		executor.submit(scan, ip, port)
+if "-sV" in argv:
+    scanV = True
+else:
+    scanV = False
+
+if "-sT" in argv:
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if scanV:
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+            for port in ports:
+                executor.submit(scanVersion, ip, port, soc)
+    else:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+            for port in ports:
+                executor.submit(scan, ip, port, soc)
+elif "-sU" in argv:
+    soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    if scanV:
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+            for port in ports:
+                executor.submit(scanVersion, ip, port, soc)
+
+    else:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+            for port in ports:
+                executor.submit(scan, ip, port, soc)
+else:
+    print("Please use -sT or -sU\nQUITTING!")
+    exit(-1)
+
+
 
 td = time() - ts
 
